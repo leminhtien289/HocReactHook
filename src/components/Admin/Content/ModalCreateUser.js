2ccaffd4ea0b47f6e126cbd78d16e807b03e393d
lolicon <leminhtien2892004@gmail.com>
2025-04-30 14:04:19 +0700

    Validate data và React Toastify

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from 'react-icons/fc';
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = ({ show, setShow }) => {
     const handleClose = () => {
@@ -29,17 +30,27 @@ const ModalCreateUser = ({ show, setShow }) => {
         }
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handleSubmitCreateUser = async () => {
         //validate
+        const isValidEmail = validateEmail(email);
 
-        //call apis
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image
-        // }
+        if (!isValidEmail) {
+            toast.error("Invalid email!");
+            return;
+        }
+
+        if (!password) {
+            toast.error("Invalid password!");
+            return;
+        }
 
         const data = new FormData();
         data.append('email', email);
@@ -50,7 +61,15 @@ const ModalCreateUser = ({ show, setShow }) => {
 
         let res = await axios.post('http://localhost:8081/api/v1/participant', data);
 
-        console.log(">>> check: ", res);
+        console.log(">>> check: ", res.data);
+        if (res.data.EC === 0) {
+            toast.success(res.data.EM);
+            handleClose();
+        }
+
+        if (res.data && res.data.EC !== 0) {
+            toast.error(res.data.EM);
+        }
 
     }
 
--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ import HomePage from './components/Home/HomePage';
 import ManageUser from './components/Admin/Content/ManageUser';
 import DashBoard from './components/Admin/Content/DashBoard';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
