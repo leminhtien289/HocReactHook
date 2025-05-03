73bf98bca9752a30549160460ec6230796815d14
lolicon <leminhtien2892004@gmail.com>
2025-05-03 11:49:39 +0700

    Loading bar - Hiển thị thanh loading khi gọi APIs

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -5,10 +5,12 @@ import { postLogin } from '../../services/apiService';
 import { toast } from 'react-toastify';
 import { useDispatch } from 'react-redux';
 import { doLogin } from '../../redux/action/userLogin';
+import { ImSpinner10 } from 'react-icons/im';
 
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
@@ -34,16 +36,20 @@ const Login = (props) => {
             return;
         }
 
+        setIsLoading(true);
+
         //Submit apis
         let data = await postLogin(email, password);
         if (data && data.EC === 0) {
             dispatch(doLogin(data));
             toast.success(data.EM);
+            setIsLoading(false);
             navigate('/')
         }
 
         if (data && +data.EC !== 0) {
             toast.error(data.EM);
+            setIsLoading(false);
         }
     }
 
@@ -88,8 +94,10 @@ const Login = (props) => {
                     <button
                         className='btn-submit'
                         onClick={() => handleLogin()}
+                        disabled={isLoading}
                     >
-                        Login to HoiDanIT
+                        {isLoading && <ImSpinner10 className='loader-icon' />}
+                        <span>Login to HoiDanIT</span>
                     </button>
                 </div>
                 <div className='text-center'>
--- src/utils/axiosCustomize.js
+++ src/utils/axiosCustomize.js
@@ -1,4 +1,10 @@
 import axios from 'axios';
+import NProgress from 'nprogress';
+
+NProgress.configure({
+    showSpinner: false, // Disable the spinner
+    trickleSpeed: 100, // Speed of the trickle effect
+});
 
 const instance = axios.create({
     baseURL: 'http://localhost:8081/',
@@ -6,6 +12,7 @@ const instance = axios.create({
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
+    NProgress.start(); // Start the progress bar
     // Do something before request is sent
     return config;
 }, function (error) {
@@ -15,6 +22,7 @@ instance.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done(); // Complete the progress bar
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response;
