5a27454c8e8252639fde3d543a4a4600cd35c2a4
lolicon <leminhtien2892004@gmail.com>
2025-06-09 11:22:26 +0700

    Countdown timer

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -147,6 +147,7 @@ const DetailQuiz = () => {
             <div className="right-content">
                 <RightContent
                     dataQuiz={dataQuiz}
+                    handleFinishQuiz={handleFinishQuiz}
                 />
             </div>
             <ModalResult
