6f2d01927ba23210e32c5df27332ff6c6fba0ba0
lolicon <leminhtien2892004@gmail.com>
2025-06-10 16:02:44 +0700

    Tích hợp api dashboard

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -117,6 +117,10 @@ const logout = (email, refresh_token) => {
     return axios.post(`api/v1/logout`, { email, refresh_token });
 }
 
+const getOverView = () => {
+    return axios.get(`api/v1/overview`);
+}
+
 export {
     postCreateNewUser,
     getAllUsers,
@@ -138,4 +142,5 @@ export {
     getQuizWithQA,
     postUpsertQA,
     logout,
+    getOverView,
 }
